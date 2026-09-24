1efc03630fc428003bc2d66a4977bfcbb95ed251
Alan Godoy <alanfull21@gmail.com>
2026-09-24 18:14:32 -0300

    Agregar estados con su interacción

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ProjectCard = ({Titulo, descripcion}) => {
+    const [mostrarMas, setMostrarMas] = useState(false)
   return (
     <article>
         <h3>{Titulo}</h3>
         <p>{descripcion}</p>
+
+        <button onClick={() => setMostrarMas(!mostrarMas)}>
+            {mostrarMas ? 'ver menos' : 'Ver más'}
+        </button>
+        {mostrarMas && (
+            <p>
+                Este proyecto me ayudo a entender fundamentos de programación en c#,
+                Permitiendome Avanzar mas en mi experiencia como desarrollador
+            </p>
+        )}
     </article>
   )
 }
